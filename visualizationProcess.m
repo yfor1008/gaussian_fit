7ea408b729bdbf9fd82cc8bb0f1fbe1433db94f1
yfor <yfor1008@gmail.com>
2020-08-21 17:29:06 +0800

    [update] update visualization
    1. add aplha for png
    2. reduce whitespace of figure

--- visualizationProcess.m
+++ visualizationProcess.m
@@ -36,10 +36,28 @@ if strcmpi(result, 'final')
     plot(x, G)
     legend_cell{end} = '拟合结果';
     legend(legend_cell)
-    [A, map] = rgb2ind(frame2im(getframe(gcf)), 256);
-    imwrite(A, map, '拟合结果.png');
+    
+    ax = gca;
+    outerpos = ax.OuterPosition;
+    ti = ax.TightInset;
+    left = outerpos(1) + ti(1);
+    bottom = outerpos(2) + ti(2);
+    ax_width = outerpos(3) - ti(1) - ti(3);
+    ax_height = outerpos(4) - ti(2) - ti(4);
+    ax.Position = [left bottom ax_width ax_height];
+    
+    set(gca, 'color', 'none');
+    fig_rgb = getframe(gcf);
+    fig_rgb = fig_rgb.cdata;
+    alpha = ones(size(fig_rgb, 1), size(fig_rgb, 2));
+    fig_gray = rgb2gray(fig_rgb);
+    alpha(fig_gray==240) = 0;
+    imwrite(fig_rgb, '拟合结果.png', 'Alpha', alpha);
+
 elseif strcmpi(result, 'process')
     figure('NumberTitle', 'off', 'Name', 'Process of Gaussian Fitting')
+    T = tiledlayout(2,1);
+
     err = outStruct.error;
     err_df = err(2:end) - err(1:end-1);
     err_df = round(err_df * 10000)/10000;
@@ -49,7 +67,7 @@ elseif strcmpi(result, 'process')
     end
 
     for frm = 1 : step : num
-        subplot(211)
+        nexttile(1)
         plot(x, y, 'ro')
         axis([0, max(x), 0, max(y)*1.2])
         legend_cell{1} = '原始数据';
@@ -70,19 +88,18 @@ elseif strcmpi(result, 'process')
         legend(legend_cell)
         hold off,
         
-        subplot(212)
-        % plot(outStruct.error(1:frm))
+        nexttile(2)
         semilogy(outStruct.error(1:frm))
-        % xlabel('iter')
-        % ylabel('error')
         
-        suptitle(['第 ', num2str(frm), ' 次迭代']);
-    
+        title(T, ['第 ', num2str(frm), ' 次迭代']);
+        T.TileSpacing = 'compact';
+        T.Padding = 'compact';
+
         [A, map] = rgb2ind(frame2im(getframe(gcf)), 256);
         if frm == 1
-            imwrite(A, map, '迭代过程.gif', 'gif', 'Loopcount',inf, 'DelayTime',0.005);
+            imwrite(A, map, '迭代过程.gif', 'gif', 'Loopcount',inf, 'DelayTime',0.05);
         else
-            imwrite(A, map, '迭代过程.gif', 'gif', 'WriteMode','append', 'DelayTime',0.005);
+            imwrite(A, map, '迭代过程.gif', 'gif', 'WriteMode','append', 'DelayTime',0.05);
         end
     end
 else
