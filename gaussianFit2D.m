f693c4bc4f0f787971406e8cee60ea5a7c4e8c68
yfor <yfor1008@gmail.com>
2020-08-27 22:40:37 +0800

    [add] add mode for 2D fitting

--- gaussianFit2D.m
+++ gaussianFit2D.m
@@ -5,15 +5,21 @@ function [outStruct] = gaussianFit2D(x, y, z, same, peakNum, iterNum)
 %   - x: m*n, 自变量
 %   - y: m*n, 自变量
 %   - z: m*n, 因变量
-%   - same: bool, x 方向和y 方向的方差是否相同, 1-相同, 0-不相同
+%   - same: int, 代表高斯核不同模式
+%           0-高斯核中心不同, xy 方向方差不同;
+%           1-高斯核中心不同, xy 方向方差相同;
+%           2-高斯核中心相同, xy 方向方差相同;
+%           3-高斯核中心相同, xy 方向方差不同;
 %   - peakNum: int, 高斯核个数
 %   - iterNum: int, 最高迭代次数
 % output:
 %   - outStruct: struct, 结果结构体
+% docs:
+%   same=2/3 时, 拟合结果存在问题!!!
 %
 
 if ~exist('iterNum', 'var')
-    iterNum = 100;
+    iterNum = 500;
 end
 if ~exist('peakNum', 'var')
     peakNum = 1;
@@ -33,13 +39,8 @@ ERRORS = zeros(iterNum, 1);
 
 % 优化参数
 options = optimset('TolX',0.0001, 'Display','off', 'MaxFunEvals',iterNum);
-if same
-    startPoint = calcStartSame(x, y, peakNum);
-    fminsearch(@(lambda)(fitgaussian2DSame(lambda, x, y, z)), startPoint, options);
-else
-    startPoint = calcStart(x, y, peakNum);
-    fminsearch(@(lambda)(fitgaussian2D(lambda, x, y, z)), startPoint, options);
-end
+startPoint = calcStart(x, y, peakNum, same);
+fminsearch(@(lambda)(fitgaussian2D(lambda, x, y, z, peakNum, same)), startPoint, options);
 
 if INDEX+1 < iterNum
     HEIGHTS(INDEX+1:end, :) = [];
@@ -60,71 +61,86 @@ outStruct.error = ERRORS;
 
 end
 
-function startPoint = calcStart(x, y, peakNum)
-% calcStart - 计算初始位置, 在取值范围内等间隔取点
+function startPoint = calcStart(x, y, peakNum, same)
+% calcStart - 计算初始位置, 迭代参数起始位置
 %
 % input:
 %   - x: m*n, 自变量
 %   - y: m*n, 自变量
 %   - peakNum: int, 高斯核个数
+%   - same: int, 代表高斯核不同模式
+%           0-高斯核中心不同, xy 方向方差不同;
+%           1-高斯核中心不同, xy 方向方差相同;
+%           2-高斯核中心相同, xy 方向方差相同;
+%           3-高斯核中心相同, xy 方向方差不同;
 % output:
-%   - startPoint: 1*(peakNum*4), 由(px, py, wx, xy)依次构成
+%   - startPoint: 迭代参数
+%       same=0 时, output 大小为 1*(peakNum*4), [px,py,wx,wy,px,py,wx,wy, ...]
+%       same=1 时, output 大小为 1*(peakNum*3), [px,py,wxy,px,py,wxy, ...]
+%       same=2 时, output 大小为 1*(peakNum*1+2), [px,py,wxy,wxy, ...]
+%       same=3 时, output 大小为 1*(peakNum*2+2), [px,py,wx,wy,wx,wy, ...]
 %
-
-ss = 4;
 
 maxXVal = max(x(:));
 minXVal = min(x(:));
 rangeX = maxXVal - minXVal;
 stepX = rangeX / (peakNum + 1);
-px = (stepX : stepX : rangeX-stepX) + minXVal;
-wx = rangeX / (3 * peakNum);
 maxYVal = max(y(:));
 minYVal = min(y(:));
 rangeY = maxYVal - minYVal;
 stepY = rangeY / (peakNum + 1);
-py = (stepY : stepY : rangeY-stepY) + minYVal;
-wy = rangeX / (3 * peakNum);
-startPoint = zeros(peakNum*ss, 1);
-startPoint(1:ss:end) = px;
-startPoint(2:ss:end) = py;
-startPoint(3:ss:end) = wx;
-startPoint(4:ss:end) = wy;
+
+if same == 0
+    px = (stepX : stepX : rangeX-stepX) + minXVal;
+    wx = rangeX / (3 * peakNum);
+    py = (stepY : stepY : rangeY-stepY) + minYVal;
+    wy = rangeY / (3 * peakNum);
+
+    ss = 4;
+    startPoint = zeros(peakNum*ss, 1);
+    startPoint(1:ss:end) = px;
+    startPoint(2:ss:end) = py;
+    startPoint(3:ss:end) = wx;
+    startPoint(4:ss:end) = wy;
+elseif same == 1
+    px = (stepX : stepX : rangeX-stepX) + minXVal;
+    wxy = rangeX / (3 * peakNum);
+    py = (stepY : stepY : rangeY-stepY) + minYVal;
+
+    ss = 3;
+    startPoint = zeros(peakNum*ss, 1);
+    startPoint(1:ss:end) = px;
+    startPoint(2:ss:end) = py;
+    startPoint(3:ss:end) = wxy;
+elseif same == 2
+    px = (maxXVal + minXVal)/2;
+    py = (maxYVal + minYVal)/2;
+    wxy = rangeX / (3 * peakNum);
+
+    ss = 1;
+    startPoint = zeros(peakNum*ss+2, 1);
+    startPoint(1) = px;
+    startPoint(2) = py;
+    startPoint(3:end) = wxy;
+elseif same == 3
+    px = (maxXVal + minXVal)/2;
+    py = (maxYVal + minYVal)/2;
+    wx = rangeX / (3 * peakNum);
+    wy = rangeY / (3 * peakNum);
+
+    ss = 2;
+    startPoint = zeros(peakNum*ss+2, 1);
+    startPoint(1) = px;
+    startPoint(2) = py;
+    startPoint(3:ss:end) = wx;
+    startPoint(4:ss:end) = wy;
+else
+    error('参数错误!');
+end
 
 end
 
-function startPoint = calcStartSame(x, y, peakNum)
-% calcStart - 计算初始位置, 在取值范围内等间隔取点
-%
-% input:
-%   - x: m*n, 自变量
-%   - y: m*n, 自变量
-%   - peakNum: int, 高斯核个数
-% output:
-%   - startPoint: 1*(peakNum*3), 由(px, py, wxy)依次构成
-%
-
-ss = 3;
-
-maxXVal = max(x(:));
-minXVal = min(x(:));
-rangeX = maxXVal - minXVal;
-stepX = rangeX / (peakNum + 1);
-px = (stepX : stepX : rangeX-stepX) + minXVal;
-wxy = rangeX / (3 * peakNum);
-maxYVal = max(y(:));
-minYVal = min(y(:));
-rangeY = maxYVal - minYVal;
-stepY = rangeY / (peakNum + 1);
-py = (stepY : stepY : rangeY-stepY) + minYVal;
-startPoint = zeros(peakNum*ss, 1);
-startPoint(1:ss:end) = px;
-startPoint(2:ss:end) = py;
-startPoint(3:ss:end) = wxy;
-
-end
-
-function err = fitgaussian2D(lambda, x, y, z)
+function err = fitgaussian2D(lambda, x, y, z, peakNum, same)
 % gaussianFit2D - 2D高斯拟合, x 和 y 方向的方差不同
 %
 % input:
@@ -132,70 +148,55 @@ function err = fitgaussian2D(lambda, x, y, z)
 %   - x: m*n, 自变量
 %   - y: m*n, 自变量
 %   - z: m*n, 因变量
+%   - peakNum: int, 高斯核个数
+%   - same: int, 代表高斯核不同模式
+%       same=0 时, lambda 大小为 1*(peakNum*4), [px,py,wx,wy,px,py,wx,wy, ...]
+%       same=1 时, lambda 大小为 1*(peakNum*3), [px,py,wxy,px,py,wxy, ...]
+%       same=2 时, lambda 大小为 1*(peakNum*1+2), [px,py,wxy,wxy, ...]
+%       same=3 时, lambda 大小为 1*(peakNum*2+2), [px,py,wx,wy,wx,wy, ...]
 % output:
 %   - err: scaler, 误差
 % docs:
 %   - 使用最小二乘计算参数
 %
 
-step = 4;
-A = zeros(numel(x), round(length(lambda)/step));
-for j = 1:length(lambda)/step
-    g = gaussian2D(x, y, lambda((j-1)*step+1), lambda((j-1)*step+2), lambda((j-1)*step+3), lambda(j*step));
+if same == 0
+    step = 4;
+    px = lambda(1:step:end);
+    py = lambda(2:step:end);
+    wx = lambda(3:step:end);
+    wy = lambda(4:step:end);
+elseif same == 1
+    step = 3;
+    px = lambda(1:step:end);
+    py = lambda(2:step:end);
+    wx = lambda(3:step:end);
+    wy = wx;
+elseif same == 2
+    step = 1;
+    px = ones(1, peakNum) * lambda(1);
+    py = ones(1, peakNum) * lambda(2);
+    wx = lambda(3:step:end);
+    wy = wx;
+elseif same == 3
+    step = 2;
+    px = ones(1, peakNum) * lambda(1);
+    py = ones(1, peakNum) * lambda(2);
+    wx = lambda(3:step:end);
+    wy = lambda(4:step:end);
+else
+    error('参数错误!');
+end
+
+A = zeros(numel(x), peakNum);
+for j = 1:peakNum
+    g = gaussian2D(x, y, px(j), py(j), wx(j), wy(j));
     g = g(:);
     A(:,j) = g;
 end
 
 Z = z(:);
 height = A \ Z;
-px = lambda(1:step:end);
-py = lambda(2:step:end);
-wx = lambda(3:step:end);
-wy = lambda(4:step:end);
-
-z1 = A * height;
-err = norm(z1 - Z);
-
-% 更新参数
-global HEIGHTS PXS PYS WXS WYS ERRORS INDEX
-INDEX = INDEX + 1;
-HEIGHTS(INDEX, :) = height';
-PXS(INDEX, :) = px;
-PYS(INDEX, :) = py;
-WXS(INDEX, :) = wx;
-WYS(INDEX, :) = wy;
-ERRORS(INDEX) = err;
-
-end
-
-function err = fitgaussian2DSame(lambda, x, y, z)
-% gaussianFit2D - 2D高斯拟合, x 和 y 方向的方差相同
-%
-% input:
-%   - lambda: 1*(peakNum*3), 由(px, py, wxy)依次构成
-%   - x: m*n, 自变量
-%   - y: m*n, 自变量
-%   - z: m*n, 因变量
-% output:
-%   - err: scaler, 误差
-% docs:
-%   - 使用最小二乘计算参数
-%
-
-step = 3;
-A = zeros(numel(x), round(length(lambda)/step));
-for j = 1:length(lambda)/step
-    g = gaussian2D(x, y, lambda((j-1)*step+1), lambda((j-1)*step+2), lambda((j-1)*step+3), lambda(j*step));
-    g = g(:);
-    A(:,j) = g;
-end
-
-Z = z(:);
-height = A \ Z;
-px = lambda(1:step:end);
-py = lambda(2:step:end);
-wx = lambda(3:step:end);
-wy = wx;
 
 z1 = A * height;
 err = norm(z1 - Z);
--- test_2DFit.m
+++ test_2DFit.m
@@ -5,6 +5,6 @@ z = 100 * exp(-((x-200)/120).^2 - ((y-300)/120).^2) + 1 * randn(size(x));
 % figure, mesh(z)
 
 % fit
-gStr = gaussianFit2D(x, y, z, 1);
+gStr = gaussianFit2D(x, y, z, 0, 2, 1000);
 % visualizationProcess2D(x, y, z, gStr, 'final'); % 最终结果
-visualizationProcess2D(x, y, z, gStr, 'process'); % 最终结果
+visualizationProcess2D(x, y, z, gStr, 'process'); % 处理过程
